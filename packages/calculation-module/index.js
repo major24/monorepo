84c4d5e149ff6ec3b259007667aef47b69f388f3
major24 <major.nalliah@gmail.com>
2019-03-16 16:03:54 -0400

    added log4js module

--- packages/calculation-module/index.js
+++ packages/calculation-module/index.js
@@ -1,8 +1,16 @@
 const addModule = require('addition-module');
 const subModule = require('subtraction-module');
 
-console.log('testing lerna monorepo...')
+console.log('testing lerna monorepo...');
+console.log('Testing external modules stored at root, can be used in internal modules..');
+
 console.log('adding 5 + 4 = ', addModule.add(5, 4));
 console.log('subtract 7 - 3 = ', subModule.subtract(7, 3));
+
+
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+logger.level = 'debug';
+logger.debug("From calculation module..");
 
 console.log('done');
--- packages/calculation-module/node_modules/subtraction-module/index.js
+++ packages/calculation-module/node_modules/subtraction-module/index.js
@@ -1,3 +1,9 @@
 module.exports.subtract = function(a, b) {
+
+    var log4js = require('log4js');
+    var logger = log4js.getLogger();
+    logger.level = 'debug';
+    logger.debug("From subtraction module..");
+
     return a - b;
 }
